50757c711966ffe541f5a561ac806daef9f32dda
NunezHoracio <nunezhoraciodev@gmail.com>
2025-09-25 15:59:44 -0300

    Pantalla de Inicio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+       <div className="flex justify-center items-center h-screen bg-gray-900 text-white font-mono">
+      <h1 className="text-4xl animate-pulse">
+        Iniciando el Portafolio Retro...
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
